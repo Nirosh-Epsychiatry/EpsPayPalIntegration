c91d9a8c68f8d058585503fd134e84ca4d06c540
kpnlakmal <64244041+kpnlakmal@users.noreply.github.com>
2021-03-24 13:19:24 +0530

    Added the project

---
+++ src/main/java/com/paypal/EpspaypalApplication.java
@@ -0,0 +1,13 @@
+package com.paypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpspaypalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpspaypalApplication.class, args);
+    }
+
+}
